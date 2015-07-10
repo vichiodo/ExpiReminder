6ddbb9974a105e0aa9ec159e3251fcd2e4ee0b5c
LeonardoBrunassi <leo.brunassi.apple@hotmail.com>
2015-07-10 13:11:23 -0300

    Tretas! haha

--- ExpiReminder/ExpiReminder/verProdutoViewController.swift
+++ ExpiReminder/ExpiReminder/verProdutoViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import EventKit
 
 class verProdutoViewController: UITableViewController {
 
@@ -16,10 +17,14 @@ class verProdutoViewController: UITableViewController {
     @IBOutlet weak var lblDiasRestantes: UILabel!
     
     @IBOutlet weak var backgroundView: UIView!
+    
+    var eventStore: EKEventStore!
 
     var i : Int! = 1
     
     var produto: Array<Produto>!
+    
+    let usuarioManager = UsuarioManager.sharedInstance
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -64,8 +69,13 @@ class verProdutoViewController: UITableViewController {
         let alerta: UIAlertController = UIAlertController(title: "Atenção!", message: "Você tem certeza que deseja apagar este produto?", preferredStyle: .Alert)
         
         let ok: UIAlertAction = UIAlertAction(title: "Ok", style: .Default) { action -> Void in
+            self.cancelarNotificacao(self.produto[self.i])
+            self.excluirEventoCalendario(self.produto[self.i])
             ProdutoManager.sharedInstance.removerProduto(self.i)
-            ProdutoManager.sharedInstance.salvarProduto()
+            
+            //self.produto.removeAtIndex(self.i)
+            
+            //ProdutoManager.sharedInstance.salvarProduto()
             self.navigationController?.popViewControllerAnimated(true)
         }
         alerta.addAction(ok)
@@ -89,6 +99,45 @@ class verProdutoViewController: UITableViewController {
         super.didReceiveMemoryWarning()
     }
     
+    
+    func cancelarNotificacao(prod: Produto) {
+        for i in 0...usuarioManager.getDiasAlerta() {
+            var localNotification:UILocalNotification = UILocalNotification()
+            localNotification.alertAction = "Produto vencendo"
+            var diasRestantes = 7 - i
+            var strNotif = "\(prod.nome)"
+            if diasRestantes == 0 {
+                localNotification.alertBody = "'\(strNotif)' vai vencer hoje!"
+            }
+            else if diasRestantes == 1 {
+                localNotification.alertBody = "'\(strNotif)' vai vencer amanhã!"
+            }
+            else {
+                localNotification.alertBody = "Faltam \(diasRestantes) dias para '\(strNotif)' vencer!"
+            }
+            
+            let dateFix: NSTimeInterval = floor(prod.dataValidade.timeIntervalSinceReferenceDate / 60.0) * 60.0 * 24
+            var horario: NSDate = NSDate(timeIntervalSinceReferenceDate: dateFix)
+            
+            let intervalo: NSTimeInterval = -NSTimeInterval(60*60*24 * (diasRestantes))
+            
+            localNotification.soundName = UILocalNotificationDefaultSoundName
+            localNotification.applicationIconBadgeNumber = 1
+            
+            localNotification.fireDate = NSDate(timeInterval: intervalo, sinceDate: horario)
+            UIApplication.sharedApplication().cancelLocalNotification(localNotification)
+        }
+    }
+    
+    func excluirEventoCalendario(prod: Produto){
+        var endData: NSDate = NSDate(timeInterval: 3600, sinceDate: prod.dataValidade)
+        var predicate = eventStore.predicateForEventsWithStartDate(prod.dataValidade, endDate: endData, calendars:[eventStore.defaultCalendarForNewEvents])
+        var eventos = eventStore.eventsMatchingPredicate(predicate)
+        eventStore.removeEvent((eventos.last as! EKEvent), span: EKSpanThisEvent, error: NSErrorPointer())
+        
+        
+    }
+
     
     // MARK: - Table view data source
     //por conta de estar ativo ou não, temos que controlar o numero de sections e  cells, dai tem que fazer na mão, mas se não fosse isso, a propria tableview estatica da conta do trabalho
